25fbb2e39fcaba6c042560f53dc3515d59e9ee34
mjkuranda <kuranda.marek@gmail.com>
2025-03-22 15:12:44 +0100

    Tests

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,18 +1,95 @@
-describe('Swiper Gallery Test', function () {
-  it('Checks if second slide contains "United Kingdom"', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.swiper-button-next').click();
-    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
-  });
-});
+// describe("Swiper Gallery Test", function () {
+//     it('Checks if second slide contains "United Kingdom"', function () {
+//         cy.visit("http://localhost:3001");
+//         cy.get(".swiper-button-next").click();
+//         cy.get(".swiper-slide-active").should("contain", "United Kingdom");
+//     });
+// });
 
-describe('Swiper Gallery Test', function () {
-  it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.swiper-button-next').click();
-    cy.wait(2000);
-    cy.get('.swiper-button-next').click({ force: true });
-    cy.wait(2000);
-    cy.get('.swiper-slide-active').should('contain', 'Paris');
-  });
+// describe("Swiper Gallery Test", function () {
+//     it('Checks if third slide contains "Paris"', function () {
+//         cy.visit("http://localhost:3001");
+//         cy.get(".swiper-button-next").click();
+//         cy.wait(2000);
+//         cy.get(".swiper-button-next").click({ force: true });
+//         cy.wait(2000);
+//         cy.get(".swiper-slide-active").should("contain", "Paris");
+//     });
+// });
+
+// describe("Sliding Mechanism Test", function () {
+//     it("Checks if can go back and forth", function () {
+//         const mockFirstSlide = { title: "Rome", description: "Italy" };
+//         const mockSecondSlide = {
+//             title: "London",
+//             description: "United Kingdom",
+//         };
+
+//         cy.visit("http://localhost:3001");
+//         cy.get(".swiper-slide-active")
+//             .should("contain", mockFirstSlide.title)
+//             .should("contain", mockFirstSlide.description);
+//         cy.get(".swiper-button-next").click();
+//         cy.wait(2000);
+//         cy.get(".swiper-slide-active")
+//             .should("contain", mockSecondSlide.title)
+//             .should("contain", mockSecondSlide.description);
+//         cy.get(".swiper-button-prev").click();
+//         cy.wait(2000);
+//         cy.get(".swiper-slide-active")
+//             .should("contain", mockFirstSlide.title)
+//             .should("contain", mockFirstSlide.description);
+//     });
+// });
+
+// describe("Title and Description Test", function () {
+//     it("Checks if title and description are consistent with requirements", function () {
+//         const mockRequiredSlides = [
+//             { title: "Rome", description: "Italy" },
+//             { title: "London", description: "United Kingdom" },
+//             { title: "Paris", description: "France" },
+//         ];
+
+//         cy.visit("http://localhost:3001");
+
+//         for (const mockSlide of mockRequiredSlides) {
+//             cy.get(".swiper-slide-active .card-description h1").should(
+//                 "contain",
+//                 mockSlide.title
+//             );
+//             cy.get(".swiper-slide-active .card-description p").should(
+//                 "contain",
+//                 mockSlide.description
+//             );
+//             cy.get(".swiper-button-next").click();
+//             cy.wait(2000);
+//         }
+//     });
+// });
+
+describe("Responsiveness Test", function () {
+    it("Checks different viewports", function () {
+        const viewports = [
+            { device: "macbook-11", expectedWidth: 352 },
+            { device: "iphone-8", expectedWidth: 280 },
+            { device: "ipad-mini", expectedWidth: 384 },
+        ];
+
+        viewports.forEach(({ device, expectedWidth }) => {
+            it(`Should set correct slide width on ${device}`, function () {
+                cy.viewport(device);
+                cy.visit("http://localhost:3001");
+
+                cy.get(".swiper-slide")
+                    .first()
+                    .should("have.css", "width", `${expectedWidth}px`);
+
+                cy.get(".swiper-button-prev").should("be.visible");
+                cy.get(".swiper-button-prev").click();
+
+                cy.get(".swiper-button-next").should("be.visible");
+                cy.get(".swiper-button-next").click();
+            });
+        });
+    });
 });
